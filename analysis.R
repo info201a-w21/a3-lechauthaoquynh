66ef71d0ed46dd61d70a38ad5d575b4afbde2741
lechauthaoquynh <lechauthaoquynh@gmail.com>
2021-03-01 23:07:39 -0800

    resubmit

--- analysis.R
+++ analysis.R
@@ -11,23 +11,24 @@ library(mapproj)
 library(patchwork)
 
 # Load incarceration_trends.csv file
-incarceration_trends <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")  
+incarceration_trends <- read.csv(
+  "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")  
 
 # Variable: Black Jail Population ----------------------------------------------
-#What is the average value of black jail population across all the counties 
-# most recently? `recent_average_black_jail_pop`
+#What is the average value of black jail population across all the counties most 
+#recently? `recent_average_black_jail_pop`
 recent_average_black_jail_pop <- incarceration_trends %>%
   filter(year == max(year, na.rm = T)) %>%
   summarize(mean_black_jail = mean(black_jail_pop, na.rm = T))
 
-# When is the highest black jail population from the dataset? 
+# When is the highest black jail population from the dataset?
 #`year_highest_black_jail_pop`
 year_highest_black_jail_pop <- incarceration_trends %>%
   filter(black_jail_pop == max(black_jail_pop, na.rm = T)) %>%
   pull(year)
 
 # Where is the highest black jail population from the dataset?
-# `place_highest_black_jail_pop`
+#`place_highest_black_jail_pop`
 place_highest_black_jail_pop <- incarceration_trends %>%
   filter(black_jail_pop == max(black_jail_pop, na.rm = T)) %>%
   select(state, county_name)
@@ -54,14 +55,14 @@ black_jail_pop_over_time_in_top_5 <- ggplot(data = top_5_chart) +
   geom_point(mapping = aes(x = year, y = black_jail_pop, color = county_name)) +
   geom_smooth(mapping = aes(x = year, y = black_jail_pop, color = county_name),
               se = FALSE) +
-  labs(x = "Year", y = "Black Jail Population", title = "Top 5 Counties Has Highest
-  Black Jail Population Over Time") +
+  labs(x = "Year", y = "Black Jail Population", title = "Top 5 Counties Has 
+       Highest Black Jail Population Over Time") +
   scale_color_discrete("County Name")
 
 # Variable comparison charts--------------------------------------------------
 #Make a data frame for black and white jail pop? `two variables data`
-two_variables_data <- data.frame(
-  incarceration_trends$black_jail_pop, incarceration_trends$white_jail_pop)
+two_variables_data <- data.frame(incarceration_trends$black_jail_pop,
+                                 incarceration_trends$white_jail_pop)
 
 #Graph the two variables comparison chart? `variables_comparison_chart`
 variable_comparison_chart <- ggplot(data = two_variables_data) +
@@ -81,7 +82,7 @@ join_map <- map_data("county") %>%
   unite(polyname, region, subregion, sep = ",") %>%
   left_join(county.fips, by = "polyname")
 
-#Merge map data and incarceration data? `merge_map`
+#Merge map data and incarceration data?`merge_map`
 merge_map <- join_map %>%
   left_join(black_jail_data, by = "fips") %>%
   filter(black_jail_pop != "NA") 
@@ -103,11 +104,9 @@ blank_theme <- theme_bw() +
 black_jail_map <- ggplot(merge_map) +
   geom_polygon(
     mapping = aes(x = long, y = lat, group = group, fill = black_jail_pop),
-    color = "gray", size = 0.3
-  ) +
+    color = "gray", size = 0.3) +
   coord_map() +
   scale_fill_continuous(limits = c(0, max(merge_map$black_jail_pop)), na.value = 
                           "white", low = "yellow", high = "red") +
   blank_theme +
-  ggtitle("Black Jail Population in the U.S") 
-  
+  ggtitle("Black Jail Population in the U.S")
